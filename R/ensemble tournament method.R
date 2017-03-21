f3f533f4658b011a2cb0cea190956a2aa5d97d3b
Matt <matthewdavis.wi@gmail.com>
2017-03-20 22:56:37 -0500

    Cleaning up for easier interpretation

--- R/ensemble tournament method.R
+++ R/ensemble tournament method.R
@@ -1,51 +1,50 @@
-# Pomeroy ordinals test ----------
-    # Get pomeroy results
-pomeroy <- massey_ordinals_2003_2016 %>% 
-  filter(sys_name == 'POM',
-         rating_day_num == 133)
+# Load Libraries -------
+library(readr); library(dplyr); library(tidyr); library(reshape)
 
-pomeroy.17 <- Massey_ordinals_2017 %>% 
-  filter(sys_name == 'POM',
-         rating_day_num == 133)
+# Load Data -----------
+seasons <- read_csv("../data/Seasons.csv")
+teams <- read_csv("../data/Teams.csv")
+TourneyCompactResults <- read_csv("../data/TourneyCompactResults.csv")
+seeds <- read_csv("../data/TourneySeeds.csv")
+submission <- read_csv("../data/SampleSubmission.csv")
+massey_ordinals_2003_2016 <- read_csv('../data/massey_ordinals_2003-2016.csv')
+Massey_ordinals_2017 <- read_csv('../data/Massey_ordinals_2017.csv')
 
-    # Make one data frame of Pomeroy
-pom <- rbind(pomeroy, pomeroy.17)
-rm(pomeroy); rm(pomeroy.17)
+# Ensemble tournament ordinals test ----------
+# Test all systems
+ensemble <- massey_ordinals_2003_2016 %>% 
+  filter(rating_day_num == 133)
+ensemble.wide <- spread(ensemble, sys_name, orank)
+ensemble.17 <- Massey_ordinals_2017 %>% 
+  filter(rating_day_num == 133)
+ensemble.17.wide <- spread(ensemble.17, sys_name, orank)
 
-    # Make training and test data sets from compact results
-ordinals.data <- compactSeasonData %>% 
-  filter(Season >= 2003)
+    # Create data frame of games to predict
+games.to.predict <- cbind(submission$Id, colsplit(submission$Id, split = "_", names = c('season', 'team1', 'team2')))  
 
-
-submission.2016 <- fread('2016submission.csv')
-games.to.predict <- cbind(submission.2016$Id, colsplit(submission.2016$Id, split = "_", names = c('season', 'team1', 'team2')))  
-
+    # Subset tournament data to only include years of which we have massey ordinals data
 ordinals.tourney.data <- TourneyCompactResults %>% 
   filter(Season >= 2003)
 
 ordinals.tourney.data$team1 <- ifelse(ordinals.tourney.data$Wteam > ordinals.tourney.data$Lteam, ordinals.tourney.data$Lteam, ordinals.tourney.data$Wteam) #If the ID of the winning team is higher than the ID of the losing team, team1 is the losing team, else its the winning team
 ordinals.tourney.data$team2 <- ifelse(ordinals.tourney.data$Wteam > ordinals.tourney.data$Lteam, ordinals.tourney.data$Wteam, ordinals.tourney.data$Lteam) #Vice versa to find team2
 ordinals.tourney.data$team1Victory <- ifelse(ordinals.tourney.data$Wteam == ordinals.tourney.data$team1, 1, 0)
-seeds$pureSeed <- as.numeric(substr(seeds$Seed, 2,3))
 
-train <- merge(ordinals.tourney.data, pomeroy[,c("season", "team", "orank")], by.x = c("Season","team1"), by.y = c("season","team"))
-colnames(train)[12] <- 'team1.pom'
-train <- merge(train, pomeroy[,c('season', 'team', 'orank')], by.x = c('Season', 'team2'), by.y = c('season', 'team'))
-colnames(train)[13] <- 'team2.pom'
-train <- merge(train, seeds[,c('Season', 'Team', 'pureSeed')], by.x = c('Season', 'team1'), by.y = c('Season', 'Team'))
-colnames(train)[14] <- 'team1.pureseed'
-train <- merge(train, seeds[,c('Season', 'Team', 'pureSeed')], by.x = c('Season', 'team2'), by.y = c('Season', 'Team'))
-colnames(train)[15] <- 'team2.pureseed'
+# Merge data sets together
+train <- merge(ordinals.tourney.data, ensemble.wide[,c(1, 3:135)], by.x = c('Season', 'team1'), by.y = c('season','team'))
+names(train)[12:143] <- paste('team1.', names(train)[12:143], sep = '')
+train <- merge(train, ensemble.wide[,c(1, 3:135)], by.x = c('Season', 'team1'), by.y = c('season', 'team'))
+names(train)[144:275] <- paste('team2.', names(train)[144:275], sep = '')
 
+# Create test and train data sets
 test <- train %>% 
-  filter(Season == 2016)
+  filter(Season >= 2016)
 train <- train %>% 
-  filter(Season <= 2016)
+  filter(Season <= 2015)
 
-
-lr_model <- glm(team1Victory ~ team1.pom + team2.pom
-                , family=binomial(link = 'logit'), data = train)
-summary(lr_model)
+      # THIS IS WHERE I STOPPED 3.20.2017
+lr_model <- glm(team1Victory ~ ., 
+                family=binomial(link = 'logit'), data = train, na.action = na.exclude)
 
 #Test it on the test dataset
 test_prediction <- predict(lr_model, test, type = 'response')
@@ -57,93 +56,11 @@ sum(test$correct)/67
 
 test$prediction <- test_prediction
 test$submissionID <- paste(test$Season, test$team1, test$team2, sep = "_")
-```
-getwd()
-## Merge predictions into Submission and write it to a file
-```{r}
-submission <- merge(games.to.predict, test[,c("submissionID","prediction")], by.x = c("submission.2016$Id"), by.y = c("submissionID"), all.x = TRUE)
-submission$pred <- ifelse(is.na(submission$prediction),.5, submission$prediction)
-submission$prediction <- NULL
-submission <- submission[,c(1,5)]
-write.csv(submission, file="pomeroy2016.csv", row.names=FALSE)
-# Create bracket
-set.seed(6)
-library('kaggleNCAA')
-# You MUST move your submission file into the kaggleNCAA package folder location!!
-#(C:\Users\Matthew\Documents\R\win-library\3.3\kaggleNCAA)
-f <- system.file('LogisticRegressionSubmission.csv', package = "kaggleNCAA")
-f <- system.file('pomeroy2016.csv', package = 'kaggleNCAA')
-dat <- parseBracket(f)
-sim <- simTourney(dat, 100, year = 2016, progress=TRUE)
-bracket <- extractBracket(sim)
-printableBracket(bracket)
 
-setwd('C:\\Users\\mattd\\Dropbox\\NCAAM')
-
-# Create unique output filename
-output_filename <- '2016 kenpom and seed 100.jpeg'
-
-# Open the file for the plot to be written to
-jpeg(output_filename, height = 2400, width = 2400, res = 300, quality = 400)
-
-printableBracket(bracket)
-dev.off()
-
-#Do it for 2017
-games.to.predict <- cbind(SampleSubmission$Id, colsplit(SampleSubmission$Id, split = "_", names = c('season', 'team1', 'team2')))  
-
-test <- merge(games.to.predict, pomeroy.17[,c("season", "team", "orank")], by.x = c("season","team1"), by.y = c("season","team"))
-colnames(test)[5] <- 'team1.pom'
-test <- merge(test, pomeroy.17[,c('season', 'team', 'orank')], by.x = c('season', 'team2'), by.y = c('season', 'team'))
-colnames(test)[6] <- 'team2.pom'
-
-#Test it on the test dataset
-test_prediction <- predict(lr_model, test, type = 'response')
-
-#Compute practical accuracy measure (betting on a win or a loss)
-test$guess <- round(test_prediction, digits=0)
-test$correct <- ifelse(test$guess == test$team1Victory, 1, 0)
-sum(test$correct)/67
-
-test$prediction <- test_prediction
-test$submissionID <- paste(test$season, test$team1, test$team2, sep = "_")
-
-submission <- merge(games.to.predict, test[,c("submissionID","prediction")], by.x = c("SampleSubmission$Id"), by.y = c("submissionID"), all.x = TRUE)
-submission$pred <- ifelse(is.na(submission$prediction),.5, submission$prediction)
-submission$prediction <- NULL
-submission <- submission[,c(1,5)]
-colnames(submission)[1] <- 'id'
-write.csv(submission, file="pomeroy2016.csv", row.names=FALSE)
-?set.seed()
-set.seed(69)
-library('kaggleNCAA')
-# You MUST move your submission file into the kaggleNCAA package folder location!!
-#(C:\Users\Matthew\Documents\R\win-library\3.3\kaggleNCAA)
-f <- system.file('pomeroy2016.csv', package = 'kaggleNCAA')
-dat <- parseBracket(f)
-sim <- simTourney(dat, 1000, year = 2017, progress=TRUE)
-bracket <- extractBracket(sim)
-printableBracket(bracket)
-
-setwd('C:\\Users\\mattd\\Dropbox\\NCAAM')
-
-# Create unique output filename
-output_filename <- '2017 kenpom 1000.jpeg'
-
-# Open the file for the plot to be written to
-jpeg(output_filename, height = 2400, width = 2400, res = 300, quality = 400)
-
-printableBracket(bracket)
-dev.off()
-
-
-# Ensemble tournament ordinals test ----------
-# Get pomeroy results
+# Specific systems in use
 ensemble <- massey_ordinals_2003_2016 %>% 
   filter(rating_day_num == 133,
          sys_name %in% c('BOB', 'MOR', 'RTH', 'SAG', 'WOL', 'POM', 'WLK', 'DOL', 'COL', 'RPI'))
-
-ensemble.wide <- spread(ensemble, sys_name, orank)
 
 ensemble.17 <- Massey_ordinals_2017 %>% 
   filter(rating_day_num == 133, 
